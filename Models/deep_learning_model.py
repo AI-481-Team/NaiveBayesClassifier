5707005a501d60beb5f2d1856660aa34d078264b
Benny (mac) <benny@rseidel.de>
2024-05-05 23:09:38 -0700

    results workiing so far

--- Models/deep_learning_model.py
+++ Models/deep_learning_model.py
@@ -9,13 +9,13 @@ class KerasModel:
     This is more of a wrapper class for the Keras model from playground.ipynb
     """
     def fit(self, X, y):
-        cv = CountVectorizer()
-        X = cv.fit_transform(X).toarray()  # make it a matrix, the network can handle (not sparse matrix)
+        self.cv = CountVectorizer()
+        X_vectorized = self.cv.fit_transform(X).toarray()  # make it a matrix, the network can handle (not sparse matrix)
 
         #this will tell how the input shape of the network needs to look like
-        #print(len(cv.get_feature_names()))
+        #print(len(self.cv.get_feature_names()))
 
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+        X_train, X_test, y_train, y_test = train_test_split(X_vectorized, y, test_size=0.2, random_state=42, stratify=y)
 
         self.model = tf.keras.Sequential([
             tf.keras.layers.Dense(512, input_shape=[X_train.shape[1]], activation="relu"),
@@ -33,18 +33,20 @@ class KerasModel:
         self.history = self.model.fit(
             X_train, 
             y_train, 
-            epochs=1, 
+            epochs=3, 
             batch_size=32, 
             validation_data=(X_test, y_test)
         )
 
 
     def predict_proba(self, X):
-        probabilities = self.model.predict(X)
+        X_vectorized = self.cv.transform(X).toarray()
+        probabilities = self.model.predict(X_vectorized)
         return probabilities
 
     def predict(self,X):
-        probabilities = self.predict_proba(X)
+        X_vectorized = self.cv.transform(X).toarray()
+        probabilities = self.model.predict(X_vectorized)
         labels = (probabilities > 0.5).astype(int)
         return labels
 
